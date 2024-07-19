ffd830b32e700c941f9103c6a8c307ab6351739e
Fantom <andtorg55@yandex.ru>
2024-07-19 18:15:49 +0500

    completed work

--- module_26_fastapi/homework/main.py
+++ module_26_fastapi/homework/main.py
@@ -3,25 +3,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base, Recipe, Ingredient
 
-# Создаем экземпляр приложения FastAPI
+"""Создаем экземпляр приложения FastAPI"""
 app = FastAPI()
 
-# Подключение к базе данных SQLite
+"""Подключение к базе данных SQLite"""
 sqlalchemy_database_url = "sqlite:///./recipes.db"
 engine = create_engine(sqlalchemy_database_url)
 SessionLocal = sessionmaker(bind=engine)
 
-# Создание всех таблиц в базе данных
+"""Создание всех таблиц в базе данных"""
 Base.metadata.create_all(bind=engine)
 
-# Маршрут для получения списка всех рецептов
+"""Маршрут для получения списка всех рецептов"""
 @app.get("/recipes", response_model=list[Recipe])
 def read_recipes(skip: int = 0, limit: int = 10):
     db = SessionLocal()
     recipes = db.query(Recipe).offset(skip).limit(limit).all()
     return recipes
 
-# Маршрут для получения информации о конкретном рецепте
+"""Маршрут для получения информации о конкретном рецепте"""
 @app.get("/recipes/{recipe_id}", response_model=Recipe)
 def read_recipe(recipe_id: int):
     db = SessionLocal()
@@ -30,7 +30,7 @@ def read_recipe(recipe_id: int):
         raise HTTPException(status_code=404, detail="Recipe not found")
     return recipe
 
-# Маршрут для создания нового рецепта
+"""Маршрут для создания нового рецепта"""
 @app.post("/recipes", response_model=Recipe)
 def create_recipe(new_recipe: Recipe):
     db = SessionLocal()
--- module_26_fastapi/homework/models.py
+++ module_26_fastapi/homework/models.py
@@ -2,22 +2,29 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-# Создаем базовый класс моделей
+"""Создаем базовый класс моделей"""
 Base = declarative_base()
 
 class Recipe(Base):
     __tablename__ = 'recipes'
-
-    id = Column(Integer, primary_key=True, index=True)  # Идентификатор рецепта
-    title = Column(String)  # Название рецепта
-    time_to_cook = Column(Integer)  # Время приготовления
-    views = Column(Integer)  # Количество просмотров
-    ingredients = relationship("Ingredient", back_populates="recipe")  # Связь с ингредиентами
+    """Идентификатор рецепта"""
+    id = Column(Integer, primary_key=True, index=True)
+    """Название рецепта"""
+    title = Column(String)
+    """Время приготовления"""
+    time_to_cook = Column(Integer)
+    """Количество просмотров"""
+    views = Column(Integer)
+    """Связь с ингредиентами"""
+    ingredients = relationship("Ingredient", back_populates="recipe")
 
 class Ingredient(Base):
     __tablename__ = 'ingredients'
-
-    id = Column(Integer, primary_key=True, index=True)  # Идентификатор ингредиента
-    name = Column(String)  # Название ингредиента
-    recipe_id = Column(Integer, ForeignKey('recipes.id'))  # Внешний ключ на рецепт
-    recipe = relationship("Recipe", back_populates="ingredients")  # Связь с рецептом
+    """Идентификатор ингредиента"""
+    id = Column(Integer, primary_key=True, index=True)
+    """Название ингредиента"""
+    name = Column(String)
+    """Внешний ключ на рецепт"""
+    recipe_id = Column(Integer, ForeignKey('recipes.id'))
+    """Связь с рецептом"""
+    recipe = relationship("Recipe", back_populates="ingredients")
